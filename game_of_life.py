568c12954ef8a0fe587bd9578a3ec755518ff9b7
Charlie Patterson <pattersoncharlesl@gmail.com>
2020-04-15 18:20:42 -0400

    Fix get nieghbor coordinates method.

--- game_of_life.py
+++ game_of_life.py
@@ -1,62 +1,5 @@
 import numpy as np
 
-# Create a 2 x 2 2d matrix where values can either be 0 or 1
-board = np.random.randint(2, size=(2,2))
-# print(board)
-
-# step 1: provide x,y coordinates for a cell to evaluate
-cell_coordinates = (1,1)
-
-# step 2: get the value for that cell
-cell = board[cell_coordinates[1]][cell_coordinates[0]]
-
-board[cell_coordinates[1]][cell_coordinates[0]] = 3
-
-print(board)
-
-# print("cell",cell)
-
-# Coordinate ransformations to get neighbors
-# nw = 
-# w  = (-1,0)
-# sw = 
-# n  = (0,-1)
-# s  = (0,+1)
-# ne = 
-# e  = (+1,0)
-# se = 
-
-# How do you do addition across arrays?
-
-
-
-
- # nw = board[cell_coordinates[1] - 1][cell_coordinates[0] - 1]
- # w = board[cell_coordinates[1] - 1][cell_coordinates[0]]
- # sw = board[cell_coordinates[1] - 1][cell_coordinates[0] + 1]
- # n = board[cell_coordinates[1]][cell_coordinates[0] - 1]
- # s = board[cell_coordinates[1]][cell_coordinates[0] + 1]
- # ne = board[cell_coordinates[1] + 1][cell_coordinates[0] - 1]
- # e = board[cell_coordinates[1] - 1][cell_coordinates[0]]
- # se = board[cell_coordinates[1] - 1][cell_coordinates[0] + 1]
-# Use np.where() to get the index of a given cell
-
-
-
-# How do I evaluate a single cell? I need to find all neighboors.
-# List a cell's neighboors:
-#  Game of life uses the Moore Neighborhood 
-#  (a central cell and eight surrounding cells)
-# Neighbors are:
-# northwest
-# west
-# southwest
-# north
-# south
-# northeast
-# east
-# southeast 
-
 class Board():
 	def __init__(self,size):
 		self.board = np.random.randint(2, size=size)
@@ -66,7 +9,7 @@ class Board():
 		self.west_transform = (-1,0)
 	
 	def get_cell_value(self,cell_coordinates):
-		return self.board[cell_coordinates[1]][cell_coordinates[0]]
+		return self.board[cell_coordinates] # ndarrays take tuple indicies
 
 	def transform_coordinate(sefl, coord_one, coord_two):
 		# Add coordinates together and return result
@@ -96,36 +39,40 @@ class Board():
 		return cell_coordinates
 
 	def get_all_neighbor_coord(self,cell_coordinates):
-		neighbor_values = [self.get_neighbor_coord(cell_coordinates,direction)
+		neighbor_coordinates = [self.get_neighbor_coord(cell_coordinates,direction)
 		                   for direction in ["n","s","e","w","nw","ne","sw","se"]]
-		return neighbor_values
+		# Neighbor coordinates should never be negative                   
+		neighbor_coordinates = [coord for coord in neighbor_coordinates if all( num >= 0 for num in coord)]
+		return neighbor_coordinates
 
-	def get_live_neighbor_count(self,neighbor_coordinates):
+	def get_live_neighbor_count(self,cell_coordinates):
+		neighbor_coordinates = self.get_all_neighbor_coord(cell_coordinates)
 		live_neighbor_count = 0
 		for neighbor in neighbor_coordinates:
-			try:
-				value = self.get_cell_value(neighbor)
-				if value == 1:
-					live_neighbor_count += value
-				else:
-					continue
-			except IndexError: # Some cells don't have 8 neighbors
+			value = self.get_cell_value(neighbor)
+			if value == 1:
+				live_neighbor_count += value
+			else:
 				continue
 		return live_neighbor_count
 
 	def update_cell_value(self,cell_coordinates,new_value):
-		self.board[cell_coordinates[1]][cell_coordinates[0]] = new_value
+		self.board[cell_coordinates] = new_value
 		return None
 
-# Test Board Class
+# # Test Board Class
 
-tb = Board((2,2))
+# tb = Board((2,2))
 
-print(tb.board)
+# print(tb.board)
 
-print(tb.get_cell_value((1,1)))
+# print(tb.get_cell_value((0,0)))
 
-print(tb.get_all_neighbor_coord((1,1)))
+# print(tb.get_all_neighbor_coord((0,0)))
 
-print(tb.get_live_neighbor_count(tb.get_all_neighbor_coord((1,1))))
+# print(tb.get_live_neighbor_count((0,0)))
+
+# tb.update_cell_value((0,0), 3)
+
+# print(tb.board)
 
