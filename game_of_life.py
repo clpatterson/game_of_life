536f6c12ce0bb207ff6548746064c18812bb1c9f
Charlie Patterson <pattersoncharlesl@gmail.com>
2020-04-23 16:53:34 -0400

    Add documented first working version of game.

--- game_of_life.py
+++ game_of_life.py
@@ -1,67 +1,86 @@
+
+"""A Conway's Game of Life board class.
+
+This module provides a basic board class that tracks
+the state of the board (between generations). The board
+class provides the methods for getting a cells live
+neighbor count and updating a cells value.
+
+"""
+
 import numpy as np
 
 class Board():
-	def __init__(self,size):
-		self.board = np.random.randint(2, size=size)
-		self.north_transform = (0,-1)
-		self.south_transform = (0,1)
-		self.east_transform = (1,0)
-		self.west_transform = (-1,0)
-	
-	def get_cell_value(self,cell_coordinates):
-		return self.board[cell_coordinates] # ndarrays take tuple indicies
+    """Conway's Game of Life board."""
 
-	def transform_coordinate(sefl, coord_one, coord_two):
-		# Add coordinates together and return result
-		return tuple(map(lambda x,y: x + y, coord_one, coord_two))
+    def __init__(self, size):
+        self.board = np.random.randint(2, size=size)
+        self.north_transform = (0, -1)
+        self.south_transform = (0, 1)
+        self.east_transform = (1, 0)
+        self.west_transform = (-1, 0)
+    
+    def get_cell_value(self, cell_coordinates):
+        """Return cell value for given coordinates."""
+        return self.board[cell_coordinates] # ndarrays take tuple indicies
 
-	def get_neighbor_coord(self,cell_coordinates,direction):
-		if direction == 'n':
-			cell_coordinates = self.transform_coordinate(cell_coordinates, self.north_transform)
-		elif direction == 's':
-			cell_coordinates = self.transform_coordinate(cell_coordinates, self.south_transform)
-		elif direction == 'e':
-			cell_coordinates = self.transform_coordinate(cell_coordinates, self.east_transform)
-		elif direction == 'w':
-			cell_coordinates = self.transform_coordinate(cell_coordinates, self.west_transform)
-		elif direction == 'nw':
-			ordinal_coord = self.transform_coordinate(self.north_transform, self.west_transform)
-			cell_coordinates = self.transform_coordinate(cell_coordinates, ordinal_coord)
-		elif direction == 'ne':
-			ordinal_coord = self.transform_coordinate(self.north_transform, self.east_transform)
-			cell_coordinates = self.transform_coordinate(cell_coordinates, ordinal_coord)
-		elif direction == 'sw':
-			ordinal_coord = self.transform_coordinate(self.south_transform, self.west_transform)
-			cell_coordinates = self.transform_coordinate(cell_coordinates, ordinal_coord)
-		elif direction == 'se':
-			ordinal_coord = self.transform_coordinate(self.south_transform, self.east_transform)
-			cell_coordinates = self.transform_coordinate(cell_coordinates, ordinal_coord)
-		return cell_coordinates
+    def transform_coordinate(self, coord_one, coord_two):
+        """Return new cell coordinates after applying coordinate transformations."""
+        return tuple(map(lambda x,y: x + y, coord_one, coord_two))
 
-	def get_all_neighbor_coord(self,cell_coordinates):
-		neighbor_coordinates = [self.get_neighbor_coord(cell_coordinates,direction)
-		                   for direction in ["n","s","e","w","nw","ne","sw","se"]]
-		# Neighbor coordinates should never be negative                   
-		neighbor_coordinates = [coord for coord in neighbor_coordinates if all( num >= 0 for num in coord)]
-		return neighbor_coordinates
+    def get_neighbor_coord(self, cell_coordinates, direction):
+        """Return Moore neighbor coordinates for given direction.""" 
+        if direction == 'n':
+            cell_coordinates = self.transform_coordinate(cell_coordinates, self.north_transform)
+        elif direction == 's':
+            cell_coordinates = self.transform_coordinate(cell_coordinates, self.south_transform)
+        elif direction == 'e':
+            cell_coordinates = self.transform_coordinate(cell_coordinates, self.east_transform)
+        elif direction == 'w':
+            cell_coordinates = self.transform_coordinate(cell_coordinates, self.west_transform)
+        elif direction == 'nw':
+            ordinal_coord = self.transform_coordinate(self.north_transform, self.west_transform)
+            cell_coordinates = self.transform_coordinate(cell_coordinates, ordinal_coord)
+        elif direction == 'ne':
+            ordinal_coord = self.transform_coordinate(self.north_transform, self.east_transform)
+            cell_coordinates = self.transform_coordinate(cell_coordinates, ordinal_coord)
+        elif direction == 'sw':
+            ordinal_coord = self.transform_coordinate(self.south_transform, self.west_transform)
+            cell_coordinates = self.transform_coordinate(cell_coordinates, ordinal_coord)
+        elif direction == 'se':
+            ordinal_coord = self.transform_coordinate(self.south_transform, self.east_transform)
+            cell_coordinates = self.transform_coordinate(cell_coordinates, ordinal_coord)
+        
+        return cell_coordinates
 
-	def get_live_neighbor_count(self,cell_coordinates):
-		neighbor_coordinates = self.get_all_neighbor_coord(cell_coordinates)
-		live_neighbor_count = 0
-		for neighbor in neighbor_coordinates:
-			try:
-				value = self.get_cell_value(neighbor)
-				if value == 1:
-					live_neighbor_count += value
-				else:
-					continue
-			except IndexError:
-				continue
-		return live_neighbor_count
+    def get_all_neighbor_coord(self, cell_coordinates):
+        """Return list of all Moore neighbor coordinates for given cell."""
+        neighbor_coordinates = [ self.get_neighbor_coord(cell_coordinates, direction)
+                                 for direction in [ "n", "s", "e", "w", "nw", "ne", "sw", "se" ] ]
+        # Neighbor coordinates should never be negative                   
+        neighbor_coordinates = [ coord for coord in neighbor_coordinates 
+                                 if all( num >= 0 for num in coord) ]
+        return neighbor_coordinates
 
-	def update_cell_value(self,cell_coordinates,new_value):
-		self.board[cell_coordinates] = new_value
-		return None
+    def get_live_neighbor_count(self, cell_coordinates):
+        """Return a count of live neighbors."""
+        neighbor_coordinates = self.get_all_neighbor_coord(cell_coordinates)
+        live_neighbor_count = 0
+        for neighbor in neighbor_coordinates:
+            try:
+                value = self.get_cell_value(neighbor)
+                if value == 1:
+                    live_neighbor_count += value
+                else:
+                    continue
+            except IndexError:
+                continue
+        return live_neighbor_count
+
+    def update_cell_value(self, cell_coordinates, new_value):
+        """Update cell value with given new value."""
+        self.board[cell_coordinates] = new_value
+        return None
 
 # # Test Board Class
 
--- game_of_life_interface.py
+++ game_of_life_interface.py
@@ -1,27 +1,44 @@
-import game_of_life
+"""Conway's Game of Life interface.
+
+This module contains the user inferface for the game as well as
+the rules for how the next generation is created. The rules of the 
+game are as follows:
+
+    1) Any live cell with two or three live neighbors survives.
+    2) Any dead cell with three live neighbors becomes a live cell.
+    3) All other live cells die in the next generation. Similarly, 
+    all other dead cells stay dead.
+
+After each generation the board is updated for the user to see. 
+
+TODO: Implement as web app with Javascript (React?) frontend.
+"""
+
 import matplotlib.pyplot as plt
-from matplotlib import colors
 import numpy as np
+import game_of_life
 
 
 print("Let the Game of life begin!")
 
-# Next iteration using ndarray.view() to make changes to a view of the board
-# Then assign back to game.board
-game = game_of_life.Board((12,12)) # Start with seeded board
-plt.ion()
-img = plt.imshow(game.board)
-for n in range(100):
-	next_generation = game.board.copy()
-	for index, cell in np.ndenumerate(game.board):
-		count = game.get_live_neighbor_count(index)
-		if cell == 0 and count == 3:
-			next_generation[index] = 1 # Reproduction
-		elif cell == 1 and  count < 2 or count > 3:
-			next_generation[index] = 0 # Death from under/over population
-		else:
-			pass
-	game.board = next_generation
-	img.set_data(game.board)
-	plt.draw()
-	plt.pause(0.2)
+# TODO: allow user to select the game board size
+game = game_of_life.Board((12, 12)) # Initialize with randomly seeded board
+plt.ion() # set matplotlib to interactive mode
+img = plt.imshow(game.board) # Show the game board as image
+# TODO: allow user to select number of generations to play
+for n in range(100): # Set number of generations / life cycles to play
+    next_generation = game.board.copy()
+    for index, cell in np.ndenumerate(game.board):
+        count = game.get_live_neighbor_count(index)
+        if cell == 0 and count == 3:
+            next_generation[index] = 1 # Reproduction
+        elif cell == 1 and  count < 2 or count > 3:
+            next_generation[index] = 0 # Death from under/over population
+        else:
+            pass
+    game.board = next_generation
+    img.set_data(game.board)
+    plt.draw()
+    plt.pause(0.2) # TODO: allow user to set game speed
+
+#TODO: preserve final generation image.
