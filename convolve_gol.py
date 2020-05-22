4ad182cc941e40e20c4670b96ec17068f927b058
Charlie Patterson <pattersoncharlesl@gmail.com>
2020-05-21 22:20:56 -0400

    Clean up game version with vectorized cell evaluation.

--- convolve_gol.py
+++ convolve_gol.py
@@ -9,7 +9,8 @@ board = np.random.randint(2, size=(size,size))
 plt.ion() # set matplotlib to interactive mode
 img = plt.imshow(board)
 
-def eval_cells(cell,count):
+def eval_cells(cell, count):
+    """Evaluate and update a cell according to Conway's GOL rules."""
     if cell == 0 and count == 3:
         cell = 1 # Reproduction
     elif cell == 1 and  count < 2 or count > 3:
@@ -19,20 +20,16 @@ def eval_cells(cell,count):
     return cell
 
 veval_cells = np.vectorize(eval_cells)
-        
 
 for i in range(generations):
-    next_gen = board.copy()
-    print("starting_state:\n",next_gen)
+    previous_gen = board.copy()
     # Produce a board where each cell is filled with live neigbor count
-    neighbor_counts = (convolve2d(next_gen, np.ones((3,3)), mode="same") - next_gen)
-    print ("neighbor_counts:\n",neighbor_counts)
-    next_gen = veval_cells(next_gen,neighbor_counts)
-    print("next_gen:\n",next_gen)
+    live_neighbor_counts = (convolve2d(previous_gen, np.ones((3,3)), mode="same") - previous_gen)
+    # Evaluate each cell against Conway's COL rules to produce net generation
+    next_gen = veval_cells(previous_gen,live_neighbor_counts)
 
     # Update board visualization
     board = next_gen
-    print(board)
     img.set_data(board)
     plt.draw()
     plt.pause(.01)
